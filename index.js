917fac38258652bb3e398a865f45792841090ae5
Barthy <barthy.bonhomme@gmail.com>
2022-12-11 15:19:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { IntersectionComponent } from './src/IntersectionComponent.js'
 
 if (typeof AFRAME === 'undefined') {
-  throw new Error('Component \"remote-controls\" attempted to register before \"AFRAME\" was available.')
+  throw new Error('Component \"intersection-events\" attempted to register before \"AFRAME\" was available.')
 }
 
 AFRAME.registerComponent('intersection-events', IntersectionComponent)
